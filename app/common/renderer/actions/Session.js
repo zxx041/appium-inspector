558798253fc38d486c40e22bdd6eea1ce2ea9062
zxx041 <newstarfire@hotmail.com>
2025-04-28 10:33:55 +0800

    mjpegScreenshotUrl 端口抓取

--- app/common/renderer/actions/Session.js
+++ app/common/renderer/actions/Session.js
@@ -618,12 +618,12 @@ export function newSession(caps, attachSessId = null) {
 
     // http://127.0.0.1:9100 -> http://host:9100
     console.log("appium server host:", host);
+    console.log("mjpegScreenshotUrl before convert:", mjpegScreenshotUrl);
     if(mjpegScreenshotUrl.startsWith('http://127.0.0.1')) {
-      mjpegScreenshotUrl = 'http://127.0.0.1:9100';
       const mjpegScreenshotPort = mjpegScreenshotUrl.split(':').pop();
       mjpegScreenshotUrl = 'http://' + host + ':' + mjpegScreenshotPort;
     }
-    console.log("mjpegScreenshotUrl convert:", mjpegScreenshotUrl);
+    console.log("mjpegScreenshotUrl after convert:", mjpegScreenshotUrl);
 
     // pass some state to the inspector that it needs to build recorder
     // code boilerplate
